22bc83742bd58cbd1508396761dc0b22e92c8b4a
unknown <fegoworks@hotmail.com>
2019-11-07 14:58:13 +0100

    chore(setup): install and configure postgres

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+const {
+  Pool,
+} = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-app.use((req, res) => {
-  res.json({
-    messsage: 'Your request was successful',
-  });
+
+// DB connect string
+const connect = process.env.DB_URL;
+
+const pool = new Pool({
+  connectionString: connect,
 });
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  );
+  next();
+});
+
+app.use(bodyparser.json());
+
+// app.use('/api/v1/employees', employeeRoutes);
 
 module.exports = app;
