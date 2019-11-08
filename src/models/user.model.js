43940e14a145a1096550c5b35fa703980f8d33d1
unknown <fegoworks@hotmail.com>
2019-11-08 23:21:56 +0100

    chore(refactor): clean up codebase

---
+++ src/models/user.model.js
@@ -0,0 +1,14 @@
+class User {
+  constructor() {
+    this.firstname = null;
+    this.lastname = null;
+    this.email = null;
+    this.password = null;
+    this.gender = null;
+    this.department = null;
+    this.address = null;
+    this.jobrole = null;
+  }
+}
+
+module.exports = User;
