a808a5aa33114b2075883ad827eb8d5f05f1c8f7
unknown <fegoworks@hotmail.com>
2019-11-10 14:09:05 +0100

    feat(article): create edit resource endpoint

--- src/controllers/article.controller.js
+++ src/controllers/article.controller.js
@@ -50,6 +50,55 @@ const articleController = {
       });
     }
   },
+
+  async editArticle(req, res) {
+    const findOneQuery = 'SELECT * FROM articles WHERE articleid=$1';
+    const updateOneQuery = `UPDATE articles
+      SET title=$1,message=$2,createdon=$3,owner=$4
+      WHERE articleid=$5 returning *`;
+
+    try {
+      const {
+        rows,
+      } = await query(findOneQuery, [req.params.articleid]);
+      if (!rows[0]) {
+        return res.status(404).send({
+          message: 'article not found',
+        });
+      }
+      if (rows[0].owner !== req.id) {
+        return res.status(403).json({
+          message: 'This article was not created by you',
+        });
+      }
+      const values = [
+        req.body.title || rows[0].title,
+        req.body.message || rows[0].message,
+        new Date(),
+        req.id,
+        req.params.articleid || rows[0].articleId,
+      ];
+      const response = await query(updateOneQuery, values);
+
+
+      const {
+        title,
+        message,
+      } = response.rows[0];
+
+
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          message: 'Article updated successfully',
+          title,
+          article: message,
+        },
+      });
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  },
 };
 
 module.exports = articleController;
