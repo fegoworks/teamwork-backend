c617db0b5dd2a77afa99bf9d5a372fccc9ef3dc4
unknown <fegoworks@hotmail.com>
2019-11-13 17:46:25 +0100

    feat(routes): add validation middleware

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const articleController = require('../controllers/article.controller');
 const verifyToken = require('../middlewares/token.middleware');
+const validate = require('../middlewares/helpers/validate');
 
 const router = express.Router();
 
-router.post('/articles', verifyToken.verify, articleController.createArticle);
-router.get('/articles/:articleid', verifyToken.verify, articleController.getArticle);
-router.patch('/articles/:articleid', verifyToken.verify, articleController.editArticle);
-router.delete('/articles/:articleid', verifyToken.verify, articleController.deleteArticle);
+router.post('/articles',
+  verifyToken.verify,
+  validate.validateBody(validate.schemas.createArticleSchema),
+  articleController.createArticle);
+
+router.get('/articles/:articleid',
+  verifyToken.verify,
+  validate.validateParams(validate.schemas.articleIdSchema),
+  articleController.getArticle);
+
+router.patch('/articles/:articleid',
+  verifyToken.verify,
+  validate.validateParams(validate.schemas.articleIdSchema),
+  validate.validateBody(validate.schemas.createArticleSchema),
+  articleController.editArticle);
+
+router.delete('/articles/:articleid',
+  verifyToken.verify,
+  validate.validateParams(validate.schemas.articleIdSchema),
+  articleController.deleteArticle);
 
 module.exports = router;
--- src/routes/gif.comment.route.js
+++ src/routes/gif.comment.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const commentController = require('../controllers/gif.comment.controller');
 const verifyToken = require('../middlewares/token.middleware');
+const validate = require('../middlewares/helpers/validate');
 
 const router = express.Router();
 
-router.post('/gifs/:gifid/comments', verifyToken.verify, commentController.createComment);
+router.post('/gifs/:gifid/comments',
+  verifyToken.verify,
+  validate.validateParams(validate.schemas.gifIdSchema),
+  validate.validateBody(validate.schemas.createComment),
+  commentController.createComment);
 
 module.exports = router;
